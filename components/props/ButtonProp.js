f790ccb70f2ea815f5ac7b973fa3ea76922e0eed
Thomas <pmthomas1991@gmail.com>
2023-02-05 09:06:07 +0530

    button fix

--- components/props/ButtonProp.js
+++ components/props/ButtonProp.js
@@ -4,9 +4,9 @@
 const ButtonProp = ({icon,title,bfunc,bclassN,bdivclassN}) => {
     return (
       <div className={bdivclassN}>
-        <Button onClick={bfunc} className={bclassN}>
+        <button onClick={bfunc} className={bclassN}>
             {icon} {title}
-        </Button>
+        </button>
       </div>  
     );
 }
