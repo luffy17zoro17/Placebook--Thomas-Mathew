dc16d0d779aa05a3c240fe89ae63d56e0055ef76
Thomas <pmthomas1991@gmail.com>
2023-02-05 10:04:28 +0530

    fixed

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images:{
-    domains:[`w0.peakpx.com`,`cdn.pixabay.com`,],
+    domains:[`w0.peakpx.com`,`cdn.pixabay.com`,`images3.alphacoders.com`,`images2.alphacoders.com`,`beebom.com`],
   }
 }
 
